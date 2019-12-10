105eda6933b151e80c84fe79428c5c99a55fe69a
stainer33 <spasang91@gmail.com>
2019-12-10 17:14:52 +0545

    Area of circle done

--- app/src/main/java/com/e/assignment2/MainActivity.java
+++ app/src/main/java/com/e/assignment2/MainActivity.java
@@ -1,14 +1,51 @@
 package com.e.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.e.assignment2.fragments.AreaFragment;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+Button area, automorphing, armstrong, palindrome,SI,swapping;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        area = findViewById(R.id.area);
+        automorphing=findViewById(R.id.automorphing);
+        armstrong=findViewById(R.id.Armstrong);
+        palindrome=findViewById(R.id.palindrome);
+        SI=findViewById(R.id.SI);
+        swapping=findViewById(R.id.swapping);
+
+        area.setOnClickListener(this);
+        automorphing.setOnClickListener(this);
+        armstrong.setOnClickListener(this);
+        palindrome.setOnClickListener(this);
+        SI.setOnClickListener(this);
+        swapping.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        switch (v.getId())
+        {
+            case R.id.area:
+                AreaFragment areaFragment=new AreaFragment();
+                fragmentTransaction.replace(R.id.fragContainer,areaFragment);
+                fragmentTransaction.commit();
+
+        }
     }
 }
---
+++ app/src/main/java/com/e/assignment2/fragments/AreaFragment.java
@@ -0,0 +1,45 @@
+package com.e.assignment2.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.e.assignment2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AreaFragment extends Fragment {
+
+
+    public AreaFragment() {
+        // Required empty public constructor
+    }
+
+    Button btnCal;
+    EditText radius;
+    TextView result;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_area, container, false);
+
+        btnCal=view.findViewById(R.id.btnCal);
+        btnCal=view.findViewById(R.id.btnCal);
+        radius=view.findViewById(R.id.radius);
+        result=view.findViewById(R.id.result);
+        return view;
+
+    }
+
+}
